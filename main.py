b15181b15789e43dd30ae0dc2d02f8768c81df29
cptlazy1 <o.culhaci@gmail.com>
2025-09-06 13:36:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import re
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+zillow_url = "https://appbrewery.github.io/Zillow-Clone/"
+google_forms_url = "https://docs.google.com/forms/d/e/1FAIpQLScbEZ1vxX1uoeb8np6aCRdoEs9fGAxSE2uYMAKv6CnHU4zHtw/viewform?usp=dialog"
+
+firefox_options = webdriver.FirefoxOptions()
+firefox_options.set_preference("detach", True)
+
+zillow_driver = webdriver.Firefox(options=firefox_options)
+zillow_driver.get(zillow_url)
+
+g_forms_driver = webdriver.Firefox(options=firefox_options)
+g_forms_driver.get(google_forms_url)
+
+soup = BeautifulSoup(zillow_driver.page_source, "html.parser")
+
+# Get all the links and put them in a list
+link_list = [link['href'] for link in soup.select('a.property-card-link')]
+
+# Get all the prices and put them in a list
+rent_list = soup.find_all("span", attrs={"data-test": "property-card-price"})
+clean_rent_list = []
+for rent in rent_list:
+   parts = re.split(r"[+/]", rent.text)
+   price = parts[0].strip()
+   clean_rent_list.append(price)
+
+# Get all the addresses and put them in a list
+address_list = soup.find_all("address", attrs={"data-test": "property-card-addr"})
+clean_address_list = []
+for address in address_list:
+    clean_address_list.append(address.text.strip())
+
+# Use Selenium to fill out the Google Form
+try:
+    address_field = WebDriverWait(g_forms_driver, 3).until(
+        ec.presence_of_element_located(
+            (By.CSS_SELECTOR, 'input[aria-labelledby="i1 i4"]')
+        )
+    )
+
+    address_field.clear()
+    address_field.send_keys(clean_address_list[0])
+    rent_field = WebDriverWait(g_forms_driver, 3).until(
+        ec.presence_of_element_located(
+            (By.CSS_SELECTOR, 'input[aria-labelledby="i6 i9"]')
+        )
+    )
+    rent_field.clear()
+    rent_field.send_keys(clean_rent_list[0])
+    link_field = WebDriverWait(g_forms_driver, 3).until(
+        ec.presence_of_element_located(
+            (By.CSS_SELECTOR, 'input[aria-labelledby="i11 i14"]')
+        )
+    )
+    link_field.clear()
+    link_field.send_keys(link_list[0])
+    submit_button = WebDriverWait(g_forms_driver, 3).until(
+        ec.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Verzenden']")
+        )
+    )
+    submit_button.click()
+
+#       TODO 1: click the "Submit another response" link
+#       TODO 2: Loop through all the entries
+
+
+
+except TimeoutException:
+    print("Timeout")
+zillow_driver.quit()
+
